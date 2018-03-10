6aa4086dc32f8944f6fff1141876460567c0785a
Baback <bbknzn@gmail.com>
2018-03-10 13:52:32 +0330

    icon updated

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -10,7 +10,7 @@ import navOverview from "../dist/img/nav-overview.svg";
 import navModels from "../dist/img/nav-models.svg";
 import navDb from "../dist/img/nav-db.svg";
 import navApi from "../dist/img/nav-api.svg";
-import navAdmindashboard from "../dist/img/nav-overview.svg";
+import navAdmindashboard from "../dist/img/nav-admindashboard.svg";
 import navDocuments from "../dist/img/nav-documents.svg";
 import navMonitoring from "../dist/img/nav-monitoring.svg";
 import navStatistic from "../dist/img/nav-statistic.svg";
