f90e6112a12450d4ce507288d37b831cdd1d1ea9
Baback <bbknzn@gmail.com>
2018-03-28 22:43:14 +0430

    overview

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -34,7 +34,114 @@ class Overview extends Component {
 
     return (
       <div className="app-top">
+        <div className="overview-container">
+          <div className="overview-top">
+            <div className="t4">Welcome to haska.</div>
+            <div className="t1">Get started here.</div>
+            <div className="get-started-tips">
+              <div className="get-started-card">
+                <div className="get-started-circle">
+                  <span className="haskon-lab"></span>
+                </div>
+                <div className="get-started-text">
+                  Create your Models
+                </div>
+              </div>
+              <div className="get-started-card">
+                <div className="get-started-circle">
+                  <span className="haskon-database"></span>
+                </div>
+                <div className="get-started-text">
+                  Create your databases
+                </div>
+              </div>
+              <div className="get-started-card">
+                <div className="get-started-circle">
+                  <span className="haskon-rocket"></span>
+                </div>
+                <div className="get-started-text">
+                  Deploy your services
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overview-bottom">
+            <div className="discover-header">
+              Discover haska
+            </div>            
+            <div className="discover-card-container"> 
 
+              <div className="discover-card">
+                <div className="card-header">
+                  <div className="icon">
+                    <span className="haskon-cogs"></span>
+                  </div>
+                  <div className="title">
+                    APIs
+                  </div>
+                </div>
+                <div className="description">
+                  Far far away, behind the word mountains               
+                </div>
+                <div className="action">
+                  <Button className="app-button">Get Started</Button>
+                </div>
+              </div>
+
+              <div className="discover-card">
+                <div className="card-header">
+                  <div className="icon">
+                    <span className="haskon-dashboard"></span>
+                  </div>
+                  <div className="title">
+                    Admin Dashboard
+                  </div>
+                </div>
+                <div className="description">
+                  Far far away, behind the word mountains               
+                </div>
+                <div className="action">
+                  <Button className="app-button">Get Started</Button>
+                </div>
+              </div>
+
+              <div className="discover-card">
+                <div className="card-header">
+                  <div className="icon">
+                    <span className="haskon-display"></span>
+                  </div>
+                  <div className="title">
+                    Monitoring
+                  </div>
+                </div>
+                <div className="description">
+                  Far far away, behind the word mountains               
+                </div>
+                <div className="action">
+                  <Button className="app-button">Get Started</Button>
+                </div>
+              </div>
+
+              <div className="discover-card">
+                <div className="card-header">
+                  <div className="icon">
+                    <span className="haskon-books"></span>
+                  </div>
+                  <div className="title">
+                    Documentation
+                  </div>
+                </div>
+                <div className="description">
+                  Far far away, behind the word mountains               
+                </div>
+                <div className="action">
+                  <Button className="app-button">Get Started</Button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
